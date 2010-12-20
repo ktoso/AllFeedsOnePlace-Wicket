bf9100642ce147e8cf8c7eb3eb5a7aa7ea954a0e
Konrad Malawski <konrad.malawski@project13.pl>
2010-12-20 02:01:45 +0100

    + implemented and bound Empty Dao layer for hibernate

---
+++ src/main/java/pl/edu/agh/student/dao/FeedDaoHibernate.java
@@ -0,0 +1,28 @@
+/*
+ * This file is part of afop-stripes.
+ *
+ * afop-stripes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * afop-stripes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.edu.agh.student.dao;
+
+import pl.edu.agh.student.model.Feed;
+
+/**
+ * Date: 20.12.10
+ *
+ * @author Konrad Malawski
+ */
+public class FeedDaoHibernate extends BaseHibernateDaoImpl<Feed> implements FeedDao {
+}
